83950c1178dddfddae5518571b8f0e58467d434d
Vladislav Semenov <vladislav.semenov@gmail.com>
2009-04-06 12:27:34 +0700

    Stopped overriding Symbol class, syntax improved

--- sql_dsl.rb
+++ sql_dsl.rb
@@ -1,14 +1,21 @@
 class SqlDsl
   def construct(&block)
     @sql = []
+    @tables = {}
+    
     if block_given?
       instance_eval &block
       puts @sql
     end
   end
   
-  def method_missing(symbol, *args)
-    symbol
+  def method_missing(symbol, *args) 
+    if @tables.has_key? symbol
+      SqlTable.new(symbol)
+    else 
+      raise NameError, "Unknown method or variable #{symbol}", caller
+    end
+  
   end    
   
   def sql
@@ -19,8 +26,22 @@ class SqlDsl
     @sql << "SELECT " + fields.join(", ")
   end
   
+  
   def from(*tables)
-    @sql << "FROM " + tables.join(", ")
+    tables_processed = []
+    tables.each do |element|
+
+      if element.is_a?(Hash)
+        element.each do |real_name, pseudonym|
+          add_table(real_name, pseudonym)
+          tables_processed << "#{real_name} AS #{pseudonym}"
+        end
+      else
+        add_table(element, element)
+        tables_processed << element.to_s
+      end
+    end
+    @sql << "FROM " + tables_processed.join(", ")
   end
   
   def where(clause)
@@ -60,35 +81,81 @@ class SqlDsl
   def all
     "*"
   end  
+  
+  def _()
+  end
+    
+  def func(name, *args)
+    params = []
+    args.each do |arg|
+      if arg.is_a? SqlExpr
+        params << arg
+      else
+        params << "'#{arg}'"
+      end
+    end
+    SqlExpr.new "#{name.to_s.upcase}(#{params.join(', ')})"
+  end     
     
 # aggregate functions  
   
   def max(field)
-    :"MAX(#{field})"
+    func :max, field
   end  
   
   def min(field)
-    :"MIN(#{field})"
+    func :min, field
   end  
 
   def sum(field)
-    :"SUM(#{field})"
+    func :sum, field
   end
    
   def avg(field)
-    :"AVG(#{field})"
+    func :avg, field
   end   
     
   def count(field)
-    :"COUNT(#{field})"
+    func :count, field
   end  
+    
+private
+  def add_table(real_name, pseudonym)
+    @tables[pseudonym] = real_name
+  end    
     
 end
 
-class Symbol
+
+class SqlTable
+  def initialize(name)
+    @name = name    
+  end  
+
+  undef id, class  
+
+  def to_s
+    @name
+  end
+
+  def method_missing(symbol, *args)
+    SqlExpr.new "#{self.to_s}.#{symbol}"
+  end     
+    
+end
+
+class SqlExpr
+
+  def initialize(expr)
+    @expr = expr.to_s    
+  end
+  
+  def to_s
+    @expr
+  end
 
   def process_operator(operator, val)
-    :"#{self} #{operator} #{val}"
+    SqlExpr.new "#{self} #{operator} #{val}"
   end
 
   def as(val)
@@ -124,18 +191,17 @@ class Symbol
   end 
 
   def &(val)
-    :"(#{self}) AND (#{val})"
+    SqlExpr.new "(#{self}) AND (#{val})"
   end 
   
   def |(val)
-    :"(#{self}) OR (#{val})"
-  end 
+    SqlExpr.new "(#{self}) OR (#{val})"
+  end     
   
-  def method_missing(symbol, *args)
-    :"#{self}.#{symbol}"
-  end  
+  def desc
+    SqlExpr.new "#{self} DESC"
+  end        
   
-  undef id, class
 end  
 
 max_count = 7
@@ -143,10 +209,10 @@ max_count = 7
 sql = SqlDsl.new
 
 sql.construct do
-  select :object.if === :pid, :posts.name, :posts.title, max(:posts.rating).as(:max_rate), avg(:price), price
-  from :posts === :p
-  where (:max_rate > max_count + 6) | (:price <=> 100.5)
-  order by :name, desc(:price)
+  from :posts, :accounts => :a, :users => :users
+  select a.address, posts.name, posts.title, max(posts.rating), avg(posts.price), func(:concat, posts.name, ' * ', posts.last_name)
+  where (a.max_rate > max_count + 6) | (a.price <=> 100.5)
+  order by posts.name, a.price.desc
 end
 
 #sql.construct(:lolposts => :posts, :users => :u) do
