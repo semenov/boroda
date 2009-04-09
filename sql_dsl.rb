227b43c79b5de0b922428e2d35c053bd36d11b0c
Vladislav Semenov <vladislav.semenov@gmail.com>
2009-04-09 23:27:10 +0700

    Draft full version

--- sql_dsl.rb
+++ sql_dsl.rb
@@ -1,33 +1,71 @@
 class SqlDsl
   def construct(&block)
-    @sql = []
+    @sql_parts = {}
     @tables = {}
+    @columns = []
     
     if block_given?
       instance_eval &block
-      puts @sql
     end
   end
   
   def method_missing(symbol, *args) 
     if @tables.has_key? symbol
-      SqlTable.new(symbol)
-    else 
+      SqlTable.new(symbol, self)
+    elsif @columns.include? symbol
+      SqlExpr.new(symbol, self)
+    else
       raise NameError, "Unknown method or variable #{symbol}", caller
     end
-  
   end    
   
+  def add_column(column)
+    @columns << column
+  end   
+  
   def sql
-    @sql.join("\n")
+    processed_parts = []
+    [:select, :from, :join, :where, :group, :having, :order, :limit].each do |key|
+      if !@sql_parts.has_key? key
+        next 
+      elsif key == :join
+        processed_parts << @sql_parts[key].join("\n")
+      else
+        processed_parts << @sql_parts[key]
+      end    
+    end
+    
+    return processed_parts.join("\n")
+  end
+  
+  def to_s
+    return sql
   end
  
   def select(*fields)
-    @sql << "SELECT " + fields.join(", ")
+    @sql_parts[:select] = "SELECT " + make_column_list(fields)
   end
   
-  
   def from(*tables)
+    @sql_parts[:from] = "FROM " + make_table_list(tables) 
+  end
+  
+  def join(*tables) 
+    @sql_parts[:join] = [] if !@sql_parts.has_key? :join
+    @sql_parts[:join] << "JOIN " + make_table_list(tables)
+    return @sql_parts[:join].last
+  end
+  
+  def left(join_statement)
+    join_statement.replace "LEFT " + join_statement
+  end
+  
+  def on(clause)
+    raise "You must use 'join' before using 'on'" if !@sql_parts.has_key? :join
+    @sql_parts[:join] << "ON " + clause.to_s
+  end    
+  
+  def make_table_list(tables)
     tables_processed = []
     tables.each do |element|
 
@@ -41,32 +79,33 @@ class SqlDsl
         tables_processed << element.to_s
       end
     end
-    @sql << "FROM " + tables_processed.join(", ")
+    tables_processed.join(", ")    
   end
   
+  def make_column_list(columns)
+    columns.join(", ")
+  end  
+  
   def where(clause)
-    @sql << "WHERE " + clause.to_s
+    @sql_parts[:where] = "WHERE " + clause.to_s
   end   
   
   def having(clause)
-    @sql << "HAVING " + clause.to_s
-  end    
-  
-  def on(clause)
-    @sql << "ON " + clause.to_s
-  end   
+    @sql_parts[:having] = "HAVING " + clause.to_s
+  end     
   
   
   def limit(number, offset = 0)
-    @sql << "LIMIT"
+    @sql_parts[:limit] = "LIMIT " + number.to_s
+    
   end
   
   def order(*fields)
-    @sql << "ORDER BY " + fields.join(", ")
+    @sql_parts[:order] = "ORDER BY " + fields.join(", ")
   end   
   
   def group(*fields)
-    @sql << "GROUP BY " + fields.join(", ")
+    @sql_parts[:group] = "GROUP BY " + fields.join(", ")
   end   
 
   # Used for ORDER BY and GROUP BY
@@ -94,10 +133,12 @@ class SqlDsl
         params << "'#{arg}'"
       end
     end
-    SqlExpr.new "#{name.to_s.upcase}(#{params.join(', ')})"
+    SqlExpr.new "#{name.to_s.upcase}(#{params.join(', ')})", self
   end     
     
-# aggregate functions  
+  def raw(expr)
+    SqlExpr.new expr, self
+  end 
   
   def max(field)
     func :max, field
@@ -128,8 +169,9 @@ end
 
 
 class SqlTable
-  def initialize(name)
-    @name = name    
+  def initialize(name, parent)
+    @name = name   
+    @parent = parent 
   end  
 
   undef id, class  
@@ -139,15 +181,16 @@ class SqlTable
   end
 
   def method_missing(symbol, *args)
-    SqlExpr.new "#{self.to_s}.#{symbol}"
+    SqlExpr.new "#{self.to_s}.#{symbol}", @parent
   end     
     
 end
 
 class SqlExpr
 
-  def initialize(expr)
-    @expr = expr.to_s    
+  def initialize(expr, parent)
+    @expr = expr.to_s  
+    @parent = parent   
   end
   
   def to_s
@@ -155,11 +198,12 @@ class SqlExpr
   end
 
   def process_operator(operator, val)
-    SqlExpr.new "#{self} #{operator} #{val}"
+    SqlExpr.new "#{self} #{operator} #{val}", @parent
   end
 
   def as(val)
-    process_operator('AS', val)
+    @parent.add_column val
+    return process_operator('AS', val)
   end
   
   def ===(val)
@@ -191,16 +235,32 @@ class SqlExpr
   end 
 
   def &(val)
-    SqlExpr.new "(#{self}) AND (#{val})"
+    SqlExpr.new "(#{self}) AND (#{val})", @parent
   end 
   
   def |(val)
-    SqlExpr.new "(#{self}) OR (#{val})"
+    SqlExpr.new "(#{self}) OR (#{val})", @parent
   end     
   
   def desc
-    SqlExpr.new "#{self} DESC"
+    SqlExpr.new "#{self} DESC", @parent
   end        
+  
+  def avg
+    @parent.func :avg, self
+  end
+
+  def min
+    @parent.func :min, self
+  end
+  
+  def max
+    @parent.func :max, self
+  end
+  
+  def count
+    @parent.func :count, self
+  end    
   
 end  
 
@@ -209,11 +269,17 @@ max_count = 7
 sql = SqlDsl.new
 
 sql.construct do
-  from :posts, :accounts => :a, :users => :users
-  select a.address, posts.name, posts.title, max(posts.rating), avg(posts.price), func(:concat, posts.name, ' * ', posts.last_name)
-  where (a.max_rate > max_count + 6) | (a.price <=> 100.5)
+  from :posts, :accounts => :a, :users => :u
+  left join :avatars, :secret_passwords => :pass
+  on posts.id == avatars.post_id
+  select count(a.address).as(:adr), posts.name.count.as(:p_name), posts.title, (max(posts.rating) + 5).as(:max_posts_rating), 
+    posts.price.avg.as(:avg_price), func(:concat, posts.name, ' * ', posts.last_name)
+  where (a.max_rate > max_count + 6) | (avg_price <=> 100.5)
   order by posts.name, a.price.desc
+  limit 5
 end
+
+puts sql
 
 #sql.construct(:lolposts => :posts, :users => :u) do
 #  select posts.*, posts.id, posts.name, posts.title, posts.rating.max.as(:max_rate), post.price.avg
